54d8b2cb8c83e65761a4b7d6a2e2eb552d1ba686
unknown <glenncraig94@gmail.com>
2021-09-01 19:38:35 -0300

    Created Product entity with its variables and methods

---
+++ barService/src/main/java/entities/Product.java
@@ -0,0 +1,57 @@
+package entities;
+
+import java.util.Objects;
+
+public class Product {
+
+    private int id;
+    private String name;
+    private int price;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return getId() == product.getId() && getPrice() == product.getPrice() && getName().equals(product.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getPrice());
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
