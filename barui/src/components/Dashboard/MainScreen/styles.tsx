1c00c06a61aaf6b97473010e71f4640adec403e9
tomasf10 <tomasfrechou@uca.edu.ar>
2021-09-06 23:05:55 -0300

    CARP-19: add products button and product grid have been added

---
+++ barui/src/components/Dashboard/MainScreen/styles.tsx
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const styles = makeStyles(theme => ({
+	table: {
+		minWidth: 650,
+		background: theme.palette.action.hover,
+	},
+}));
+
+export default styles;
