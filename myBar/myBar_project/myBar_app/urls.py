51944f0cc34af9299dd0830b3168e00efe23ee0a
tomasf10 <tomasfrechou@uca.edu.ar>
2021-09-12 17:17:32 -0300

    backend folders have been reorginized

--- myBar/myBar_project/myBar_app/urls.py
+++ myBar/myBar_project/myBar_app/urls.py
@@ -1,19 +1,22 @@
 from django.urls import path
-from . import views
+from .views import userView
+from .views import productsView
 
 
 urlpatterns = [
-    path('', views.index, name="home"),
-    path('createAccount', views.user_create, name='user_creation'),
-    path('login', views.user_log_in, name='user_log_in'),
-    path('resetPassword/<int:id>', views.user_reestablish_password,
+    # user
+    path('', userView.index, name="home"),
+    path('createAccount', userView.user_create, name='user_creation'),
+    path('login', userView.user_log_in, name='user_log_in'),
+    path('resetPassword/<int:id>', userView.user_reestablish_password,
          name='reestablish_password'),
     path('updateAccountData/<int:id>',
-         views.modify_user_details, name='modify_details'),
+         userView.modify_user_details, name='modify_details'),
     path('accountDetails/<int:id>',
-         views.get_user_details, name='get_user_details'),
-    path('deleteAccount/<int:id>', views.delete_user, name='delete_user'),
-    path('addNewProduct', views.register_product, name='register_product'),
+         userView.get_user_details, name='get_user_details'),
+    path('deleteAccount/<int:id>', userView.delete_user, name='delete_user'),
+    # product
+    path('addNewProduct', productsView.register_product, name='register_product'),
     path('getAllProducts/<int:accountid>',
-         views.get_all_products, name='get_all_products'),
+         productsView.get_all_products, name='get_all_products'),
 ]
--- myBar/myBar_project/myBar_app/views/userView.py
+++ myBar/myBar_project/myBar_app/views/userView.py
@@ -7,9 +7,7 @@ from rest_framework.decorators import api_view, renderer_classes
 from rest_framework import status
 from rest_framework.response import Response
 from django.http import HttpResponse
-from .models import Mb_user
-from .models import Product
-from . import models
+from ..models.user import Mb_user
 
 
 password = openapi.Schema(title='password', type=openapi.TYPE_STRING)
@@ -126,15 +124,6 @@ def get_user_details(request, id):
         return Response(status=status.HTTP_404_NOT_FOUND)
 
 
-@swagger_auto_schema(method='get',
-                     responses={204: 'Details updated', 400: 'Invalid data'})
-@api_view(['GET'])
-def get_all_products(request, accountid):
-    product = Product.products.filter(account_id=accountid)
-
-    return Response(product.values(), status=status.HTTP_200_OK)
-
-
 @swagger_auto_schema(method='delete',
                      request_body=openapi.Schema(
                          type=openapi.TYPE_OBJECT,  # object because the data is in json format
@@ -154,31 +143,3 @@ def delete_user(request, id):
         return Response(status=status.HTTP_200_OK)
     except Exception as e:
         return Response(status=status.HTTP_404_NOT_FOUND)
-
-
-@swagger_auto_schema(method='post',
-                     request_body=openapi.Schema(
-                         type=openapi.TYPE_OBJECT,  # object because the data is in json format
-                         required=['version'],
-                         properties={
-                             'name': name,
-                             'price': price,
-                             'accountId': accountId,
-                         }
-                     ), responses={201: 'Product registration successfull', 400: 'Invalid request',  409: 'The product already exists'})
-@api_view(['POST'])
-def register_product(request):
-    try:
-        account = Mb_user.getAllUsers().filter(
-            account_id=request.data.get('accountId')).first()
-
-        product = Product(**{'name': request.data.get('name'),
-                          'price': request.data.get('price'), 'account': account})
-
-        product.save()
-        return Response(status=status.HTTP_201_CREATED)
-    except Exception as e:
-        if str(e) == "El producto ya existe":
-            return Response(status=status.HTTP_409_CONFLICT)
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
