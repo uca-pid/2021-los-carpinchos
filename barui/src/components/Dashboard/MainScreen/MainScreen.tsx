b8201d3be76031d8c471bb968b1839982d681a72
tomasf10 <tomasfrechou@uca.edu.ar>
2021-09-14 10:18:06 -0300

    CARP-37: Generic table and addButton have been created

--- barui/src/components/Dashboard/MainScreen/MainScreen.tsx
+++ barui/src/components/Dashboard/MainScreen/MainScreen.tsx
@@ -1,22 +1,11 @@
 import React from "react";
 
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import DeleteIcon from "@material-ui/icons/Delete";
-import EditIcon from "@material-ui/icons/Edit";
-import { Container, Grid, IconButton, Typography } from "@material-ui/core";
-
-import styles from "./styles";
 import CreateProduct from "./CreateProduct";
 
 import { connect } from "react-redux";
+import DataView, { ColumnDef } from "../../common/DataView/DataView";
 
-type Product = {
+export type Product = {
 	id: string;
 	name: string;
 	price: number;
@@ -26,53 +15,26 @@ type Props = {
 	products: Product[];
 };
 
-const MainScreen = ({ products }: Props) => {
-	const classes = styles();
+const columnsDef: ColumnDef[] = [
+	{
+		title: "Nombre",
+		propName: "name",
+	},
+	{
+		title: "Precio ($)",
+		propName: "price",
+		align: "right",
+	},
+];
 
+const MainScreen = ({ products }: Props) => {
 	return (
-		<Container maxWidth="md">
-			<Grid container direction="column" spacing={3}>
-				<Grid alignItems="center" container item>
-					<Grid item xs>
-						<Typography variant="h5" color="primary">
-							Productos
-						</Typography>
-					</Grid>
-					<Grid item>
-						<CreateProduct />
-					</Grid>
-				</Grid>
-				<Grid item>
-					<TableContainer component={Paper}>
-						<Table className={classes.table} aria-label="simple table">
-							<TableHead>
-								<TableRow>
-									<TableCell>Producto</TableCell>
-									<TableCell align="right">Precio ($)</TableCell>
-									<TableCell align="right">Acciones</TableCell>
-								</TableRow>
-							</TableHead>
-							<TableBody>
-								{products.map((row: Product, index: number) => (
-									<TableRow key={`${row.name}-${index}`}>
-										<TableCell align="left">{row.name}</TableCell>
-										<TableCell align="right">$ {row.price}</TableCell>
-										<TableCell align="right">
-											<IconButton disabled>
-												<DeleteIcon />
-											</IconButton>
-											<IconButton disabled>
-												<EditIcon />
-											</IconButton>
-										</TableCell>
-									</TableRow>
-								))}
-							</TableBody>
-						</Table>
-					</TableContainer>
-				</Grid>
-			</Grid>
-		</Container>
+		<DataView
+			addButton={<CreateProduct />}
+			columnsDef={columnsDef}
+			data={products}
+			title="Productos"
+		/>
 	);
 };
 
