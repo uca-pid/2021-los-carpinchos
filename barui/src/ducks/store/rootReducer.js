123e7e41a7e8daa81896a53613c99d0a30a228e8
tomasf10 <tomasfrechou@uca.edu.ar>
2021-09-14 08:42:00 -0300

    CARP-37: redux products logic has been moved to a separated file

--- barui/src/ducks/store/rootReducer.js
+++ barui/src/ducks/store/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 
 import session from "../sessionReducer";
 import notifications from "../notificationsReducer";
+import products from "../productsReducer";
 
 const rootReducer = () =>
 	combineReducers({
 		session,
 		notifications,
+		products,
 	});
 
 export default rootReducer;
