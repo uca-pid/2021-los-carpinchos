c2e45875bb3dc32c196445af4b175d5813d9b17b
Sof Teran <sofiteran98@gmail.com>
2021-10-22 21:03:40 -0300

    CARP-67: goal model, test and view added

--- myBar/myBar_project/myBar_app/models/sale.py
+++ myBar/myBar_project/myBar_app/models/sale.py
@@ -10,7 +10,7 @@ from .user import Mb_user
 
 class Sale(models.Model):
     sale_id = models.AutoField(primary_key=True)
-    creation_date = models.DateTimeField(auto_now_add=False)
+    creation_date = models.DateField(auto_now_add=False)
     account = models.ForeignKey(
         Mb_user, on_delete=models.CASCADE, default=None)
 
